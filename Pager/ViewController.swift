fb7bab1bef99ae47e3eaaa7ee203c7e6f5e14bf2
Lucas Oceano Martins <lucoceano@gmail.com>
2015-03-12 20:58:12 -0300

    Initial Commit

---
+++ Pager/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pager
+//
+//  Created by Lucas Oceano on 12/03/2015.
+//  Copyright (c) 2015 Cheesecake. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
